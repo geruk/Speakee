f9ed9b895d20368cb21044a931aca5e4d2dc6887
unknown <dnnguyen@US-SEA-R8W2F1F.ant.amazon.com>
2012-02-11 01:39:19 -0800

    Initial commit

---
+++ src/me/geruk/android/speakee/SpeakeeActivity.java
@@ -0,0 +1,13 @@
+package me.geruk.android.speakee;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeakeeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
